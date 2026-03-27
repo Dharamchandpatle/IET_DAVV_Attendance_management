8ee926ae10eba1ac55f506f9b528f6ec75c3af46
Dharamchand <dharamchandpatle917@gmail.com>
2026-03-27 16:50:55 +0530

    faculty routes tested data

---
+++ server/routes/facultyRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const FacultyController = require('../controllers/facultyController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+// Faculty auth
+router.post('/register', FacultyController.register);
+router.post('/login', FacultyController.login);
+
+// Faculty CRUD
+router.get('/', authMiddleware(['admin']), FacultyController.getAll);
+router.get('/:id', authMiddleware(['admin', 'faculty']), FacultyController.getById);
+router.put('/:id', authMiddleware(['admin', 'faculty']), FacultyController.update);
+router.delete('/:id', authMiddleware(['admin']), FacultyController.remove);
+
+module.exports = router;
