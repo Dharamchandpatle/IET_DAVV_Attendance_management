cefa445c483b2f462f633cf7eb2e5419e7815982
Dharamchand <dharamchandpatle917@gmail.com>
2026-04-07 15:25:30 +0530

    client updates

--- client/src/utils/routeLoader.js
+++ client/src/utils/routeLoader.js
@@ -1,12 +1,19 @@
 import { lazy } from 'react';
 
+const pages = import.meta.glob('../pages/**/*.jsx');
+
 export const lazyLoad = (componentPath) => {
-  const exportName = componentPath.split('/').pop().replace('.jsx', '');
-  const Component = lazy(() => 
-    import(`../pages/${componentPath}`).then(module => ({
+  const normalized = componentPath.endsWith('.jsx') ? componentPath : `${componentPath}.jsx`;
+  const exportName = normalized.split('/').pop().replace('.jsx', '');
+  const importer = pages[`../pages/${normalized}`];
+
+  if (!importer) {
+    throw new Error(`Unknown page: ${componentPath}`);
+  }
+
+  return lazy(() =>
+    importer().then((module) => ({
       default: module[exportName] || module.default
     }))
   );
-
-  return Component;
 };
