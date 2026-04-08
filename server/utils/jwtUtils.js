6a6f47863810c3a2612ce7d2a950fe6920f74c57
Dharamchand <dharamchandpatle917@gmail.com>
2026-04-09 00:01:08 +0530

    Utils

--- server/utils/jwtUtils.js
+++ server/utils/jwtUtils.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../config/env');
 
+// Generate short-lived tokens to keep sessions secure.
 exports.generateToken = (user) => {
   return jwt.sign({ id: user.id, role: user.role }, jwtSecret, { expiresIn: '1h' });
 };
